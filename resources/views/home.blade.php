e78010655ac81fe4c817d5f2b9dc0792209e561a
Axiarcu1 <giang0351@gmail.com>
2022-07-07 17:34:41 +0700

    navigation bar
    v1.1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Website</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="stylesheet1.css">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
   </head>
   <body>
     <!-- Nav bar -->
-    <header>
+    <!--<header>
         <div class="nav-container">
-            <img src="#" class="logo">
+            <img src="images/Little & Little Logo (ngang) 1.png" class="logo">
             <ul class="nav-bar">
                 <li><a href="#Trang chủ"> Trang chủ</a></li>
                 <li><a href="#Sự kiện"> Sự kiện</a></li>
                 <li><a href="#Liên hệ"> Liên hệ</a></li>
             </ul>
+        </div>
+    </header>-->
+    <header>
+        <div class="nav-container">
+            <img src="images/Little & Little Logo (ngang) 1.png" class="logo">
+            <div class="group-2">
+                <div class="frame-20">
+                    <ul class="nav-bar">
+                        <li><a href="#Trang chủ"> Trang chủ</a></li>
+                        <li><a href="#Sự kiện"> Sự kiện</a></li>
+                        <li><a href="#Liên hệ"> Liên hệ</a></li>
+                    </ul>
+                </div>
+                <div class="frame-22">
+                    <div class="group-22-1">
+                        <img src="images/Vector (Stroke).png" class="frame-21">
+                    </div>
+                    <div class="telephone">0123456789</div>
+                </div>
+            </div>
         </div>
     </header>
     <main>
