67f5cde270a74ffa05723ae980d3abd2a5c128e2
Axiarcu1 <giang0351@gmail.com>
2022-07-11 13:54:43 +0700

    routing
    adding routing for pages

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/event', function () {
+    return view('event');
+})->name('event');
+
+Route::get('/detailed', function () {
+    return view('detailed');
+})->name('detailed');
+
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
