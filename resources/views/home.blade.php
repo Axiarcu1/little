71a19128cfd5a237db313247043f9cc2e54b57b2
Axiarcu1 <giang0351@gmail.com>
2022-07-12 22:23:06 +0700

    homepage
    working on homepage and css sheet

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,43 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Website</title>
-    <link rel="stylesheet" href="stylesheet1.css">
+    <link rel="stylesheet" href="StyleSheet1.css">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,700;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
   </head>
   <body>
-    <!-- Nav bar -->
-    <!--<header>
-        <div class="nav-container">
-            <img src="images/Little & Little Logo (ngang) 1.png" class="logo">
-            <ul class="nav-bar">
-                <li><a href="#Trang chủ"> Trang chủ</a></li>
-                <li><a href="#Sự kiện"> Sự kiện</a></li>
-                <li><a href="#Liên hệ"> Liên hệ</a></li>
-            </ul>
-        </div>
-    </header>-->
     <header>
-        <div class="nav-container">
-            <img src="images/Little & Little Logo (ngang) 1.png" class="logo">
-            <div class="group-2">
-                <div class="frame-20">
-                    <ul class="nav-bar">
-                        <li><a href="#Trang chủ"> Trang chủ</a></li>
-                        <li><a href="#Sự kiện"> Sự kiện</a></li>
-                        <li><a href="#Liên hệ"> Liên hệ</a></li>
-                    </ul>
-                </div>
-                <div class="frame-22">
-                    <div class="group-22-1">
-                        <img src="images/Vector (Stroke).png" class="frame-21">
-                    </div>
-                    <div class="telephone">0123456789</div>
-                </div>
-            </div>
-        </div>
+      <div class="nav-container">
+          <img src="images/Little & Little Logo (ngang) 1.png" class="logo">
+          <ul class="nav-bar">
+              <li><a href="#Trang chủ"> Trang chủ</a></li>
+              <li><a href="#Sự kiện"> Sự kiện</a></li>
+              <li><a href="#Liên hệ"> Liên hệ</a></li>
+          </ul>
+          <div class="nav-contact">
+              <div class="nav-img">
+                  <img src="images/Vector (Stroke).png" class="nav-img-1">
+              </div>
+              <div class="telephone">0123456789</div>
+          </div>
+      </div>
     </header>
     <main>
-        <h1>Welcome to My Website</h1>  
+        <div class="background">
+          <div></div>
+        </div>
+        <img src="images/Lisa_Arnold_Lay_Do_F2 3.png" class="home-img-1">
+        <img src="images/18451 [Converted]-06 1.png" class="home-img-2">
+        <img src="images/18451 [Converted]-05 1.png" class="home-img-3">
+        <img src="images/18451 [Converted]-04 1.png" class="home-img-4">
+        <img src="images/18451 [Converted]-03 2.png" class="home-img-5">
+        <img src="images/18451 [Converted]-03 1.png" class="home-img-6">
+        <img src="images/render fix hair 1.png" class="home-img-7">
+        <img src="images/18451 [Converted]-02 1.png" class="home-img-8">
+        <img src="images/image 2.png" class="home-img-9">
+
+        <div class="box-out-1"></div>
+        <div class="box-out-2"></div>
+        <div class="box-out-3"></div>
+        <div class="text-1">Vé của bạn</div>
+
     </main>
   </body>
 </html>
