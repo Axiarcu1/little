f58e5fab84173e96b39eab6b0f0e18ca23da7ef8
Axiarcu1 <giang0351@gmail.com>
2022-07-20 00:19:03 +0700

    background
    adding pattern for background

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Website</title>
-    <link rel="stylesheet" href="StyleSheet1.css">
+    <link rel="stylesheet" href="style-little.css">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -30,8 +30,61 @@
       </div>
     </header>
     <main>
-        <div class="background">
-          <div></div>
+        <div class="background-container">
+          <div class="background-frame-1"></div>
+          <div class="background-frame-2"></div>
+          <div class="background-group-1">
+            <div class="vector-g1-1"></div>
+            <div class="vector-g1-2"></div>
+            <div class="background-group-2">
+              <div class="vector-g2-1"></div>
+              <div class="vector-g2-2"></div>
+              <div class="vector-g2-3"></div>
+              <div class="vector-g2-4"></div>
+              <div class="vector-g2-5"></div>
+              <div class="vector-g2-6"></div>
+              <div class="vector-g2-7"></div>
+              <div class="vector-g2-8"></div>
+              <div class="vector-g2-9"></div>
+              <div class="vector-g2-10"></div>
+              <div class="vector-g2-12"></div>
+              <div class="vector-g2-13"></div>
+              <div class="vector-g2-14"></div>
+              <div class="vector-g2-15"></div>
+              <div class="vector-g2-16"></div>
+              <div class="vector-g2-17"></div>
+              <div class="vector-g2-18"></div>
+              <div class="vector-g2-19"></div>
+              <div class="vector-g2-20"></div>
+              <div class="vector-g2-21"></div>
+              <div class="vector-g2-22"></div>
+              <div class="vector-g2-23"></div>
+              <div class="vector-g2-24"></div>
+              <div class="vector-g2-25"></div>
+              <div class="vector-g2-26"></div>
+              <div class="vector-g2-27"></div>
+              <div class="vector-g2-28"></div>
+              <div class="vector-g2-29"></div>
+              <div class="vector-g2-30"></div>
+              <div class="vector-g2-31"></div>
+              <div class="vector-g2-32"></div>
+              <div class="vector-g2-33"></div>
+              <div class="vector-g2-34"></div>
+              <div class="vector-g2-35"></div>
+              <div class="vector-g2-36"></div>
+              <div class="vector-g2-37"></div>
+              <div class="vector-g2-38"></div>
+              <div class="vector-g2-39"></div>
+              <div class="vector-g2-40"></div>
+            </div>
+            <div class="vector-g1-3"></div>
+            <div class="vector-g1-4"></div>
+            <div class="vector-g1-5"></div>
+            <div class="vector-g1-6"></div>
+            <div class="vector-g1-7"></div>
+            <div class="vector-g1-8"></div>
+            <div class="vector-g1-9"></div>
+          </div>
         </div>
         <img src="images/Lisa_Arnold_Lay_Do_F2 3.png" class="home-img-1">
         <img src="images/18451 [Converted]-06 1.png" class="home-img-2">
@@ -47,7 +100,6 @@
         <div class="box-out-2"></div>
         <div class="box-out-3"></div>
         <div class="text-1">Vé của bạn</div>
-
     </main>
   </body>
 </html>
